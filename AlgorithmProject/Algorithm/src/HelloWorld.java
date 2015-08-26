e3b837ada5dbf9d2a1b824843ddb50957aa209b3
amrita chowdhury <achowdhury@cafepress.com>
2015-08-25 19:32:32 -0700

    remove everything from the github

--- AlgorithmProject/Algorithm/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by achowdhury on 5/16/2015.
- */
-public class HelloWorld {
-    public  void CallHelloWorld()
-    {
-        System.out.println("Hello to the  World of Algorithms");
-    }
-}
